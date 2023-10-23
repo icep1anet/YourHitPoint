4fcf50f51187f4b575a0b9c74fc1706130c0e610
Nakao-Isshin <katyou7777@gmail.com>
2023-10-24 00:36:39 +0900

    OAuth認証完了まで

--- lib/oauth_fitbit.dart
+++ lib/oauth_fitbit.dart
@@ -1,92 +1,53 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import "package:logger/logger.dart";
-import 'package:uni_links/uni_links.dart';
 import 'package:uuid/uuid.dart';
-import 'package:oauth2/oauth2.dart' as oauth2;
+import 'package:pkce/pkce.dart';
+import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
+import 'package:http/http.dart' as http;
 
 var logger = Logger();
 
-final authorizationEndpoint =
-    Uri.parse('https://www.fitbit.com/oauth2/authorize');
-final tokenEndpoint = Uri.parse('http://www.fitbit.com/oauth2/token');
-
 const identifier = '23R5R4';
-const redirectUrl = 'https://your-hit-point-backend-2ledkxm6ta-an.a.run.app';
-final redirectUri =
-    Uri.parse('https://your-hit-point-backend-2ledkxm6ta-an.a.run.app');
+const scopes = "activity heartrate profile sleep social temperature";
+final pkcePair = PkcePair.generate(length: 96);
+//   const uuid = Uuid();
+//   final state = uuid.v4().toString();
 
-class OAuthPage extends StatelessWidget {
-  const OAuthPage({Key? key}) : super(key: key);
+final authorizationEndpoint = Uri.https('www.fitbit.com', '/oauth2/authorize', {
+  'response_type': 'code',
+  'scope': scopes,
+  'code_challenge_method': 'S256',
+  'code_challenge': pkcePair.codeChallenge,
+  'client_id': identifier,
+});
+final tokenEndpoint = Uri.https('api.fitbit.com', '/oauth2/token');
+const callbackUrlScheme = "your-hit-point-scheme";
 
-  Future<oauth2.Client> createClient() async {
-    // If we don't have OAuth2 credentials yet, we need to get the resource owner
-    // to authorize us. We're assuming here that we're a command-line application.
-    var grant = oauth2.AuthorizationCodeGrant(
-        identifier, authorizationEndpoint, tokenEndpoint);
+class FitbitAPI {
+  Future<void> callOAuth() async {
+    // OAuthを呼び出してaccess_tokenを取得する
+    // 保存はまだ
+    final result = await FlutterWebAuth2.authenticate(
+        url: authorizationEndpoint.toString(),
+        callbackUrlScheme: callbackUrlScheme);
 
-    const uuid = Uuid();
-    final state = uuid.v4().toString();
-    const scopes = ["activity", "profile", "sleep", "social", "temperature"];
-    // A URL on the authorization server (authorizationEndpoint with some additional
-    // query parameters). Scopes and state can optionally be passed into this method.
+    String code = Uri.parse(result).queryParameters["code"]!;
+    logger.d(code);
 
-    var authorizationUrl =
-        grant.getAuthorizationUrl(redirectUri, scopes: scopes, state: state);
-    if (await canLaunchUrl(Uri.parse(authorizationUrl.toString()))) {
-      await launchUrl(Uri.parse(authorizationUrl.toString()));
-    }
-
-    // ------- 8< -------
-    var responseUrl = await listen();
-
-    // Once the user is redirected to `redirectUrl`, pass the query parameters to
-    // the AuthorizationCodeGrant. It will validate them and extract the
-    // authorization code to create a new Client.
-    return await grant.handleAuthorizationResponse(responseUrl.queryParameters);
-  }
-
-  Future<Uri> listen() async {
-    StreamSubscription _sub;
-    Uri? responseUrl;
-    _sub = linkStream.listen((String? uri) {
-      if (uri.toString().startsWith(redirectUrl)) {
-        logger.d(uri);
-        responseUrl = Uri.parse(uri!);
-      }
+    final response = await http.post(tokenEndpoint, headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }, body: {
+      'client_id': identifier,
+      'code': code,
+      'code_verifier': pkcePair.codeVerifier,
+      'grant_type': 'authorization_code',
     });
-    logger.d(responseUrl);
-    return responseUrl!;
+
+    logger.d(response.body);
+
+    return;
   }
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('URL Launcher'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                var client = await createClient();
-                // if (await canLaunchUrl(url)) {
-                //   launchUrl(url);
-                // } else {
-                //   // ignore: avoid_print
-                //   print("Can't launch $url");
-                // }
-              },
-              child: const Text('Web Link'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
