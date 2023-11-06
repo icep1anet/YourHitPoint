3d08ade17d02989c6bae00d742f4a6b698e037d7
Nakao-Isshin <katyou7777@gmail.com>
2023-11-07 06:27:47 +0900

    不要なファイルの削除

--- lib/view_model/user_data_notifier.dart
+++ lib/view_model/user_data_notifier.dart
@@ -93,4 +93,38 @@ class UserDataNotifier extends StateNotifier<UserDataState> {
   Future<void> refreshUserID(String id) async {
     await setItemToSharedPref(["userId"], [id]);
   }
+
+  
+// Future<Map> loginFirebase(isRegistered, inputEmail, inputPassword) async {
+//   isRegistered = true;
+//   logger.d("login start");
+//   var url = Uri.https("vignp7m26e.execute-api.ap-northeast-1.amazonaws.com",
+//       "/default/firebase_login_yourHP", {
+//     "email": inputEmail,
+//     "password": inputPassword,
+//   });
+//   try {
+//     var response = await http.get(url);
+//     logger.d("register.body: ${response.body}");
+//     if (response.statusCode == 200) {
+//       // リクエストが成功した場合、レスポンスの内容を取得して表示します
+//       var responseMap = jsonDecode(response.body);
+//       userId = responseMap["userId"];
+//       logger.d(userId);
+//       // if (!mounted) return;
+//       setItemToSharedPref(["userId", "userName", "avatarName", "avatarType"],
+//           [userId!, userName, avatarName, avatarType]);
+//     } else {
+//       // リクエストが失敗した場合、エラーメッセージを表示します
+//       logger.d("Request failed with status: $response");
+//       isRegistered = false;
+//       return {"isCompleted": false, "error": response};
+//     }
+//     return {"isCompleted": true};
+//   } catch (e) {
+//     isRegistered = false;
+//     return {"isCompleted": false, "error": e};
+//   }
+// }
+  
 }
