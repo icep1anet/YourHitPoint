6445b5d1a92ae87528378b9841ed2d47495d38b4
Nakao-Isshin <katyou7777@gmail.com>
2023-11-10 08:19:36 +0900

    firebaseの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,19 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:flutter/material.dart";
 import "package:logger/logger.dart";
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'view/start.dart';
+import 'firebase_options.dart';
 
 const locale = Locale("ja", "JP");
 var logger = Logger();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
