6a0d619db7d0ba61b2c28c4f3c86fa04d9b8c970
Nakao-Isshin <katyou7777@gmail.com>
2023-12-01 11:53:06 +0900

    utf8でdecodeしてからjsonDecode

--- lib/view_model/HP_notifier.dart
+++ lib/view_model/HP_notifier.dart
@@ -237,8 +237,9 @@ class HPNotifier extends StateNotifier<HPState> {
     url = url.replace(queryParameters: body);
     final bodyEncoded = jsonEncode(body);
     var response = await request(url: url, type: "get", body: bodyEncoded);
+    String utf8DecodedresponseBody = utf8.decode(response.bodyBytes);
     //リクエストの返り値をマップ形式に変換
-    var responseBody = jsonDecode(response.body);
+    var responseBody = jsonDecode(utf8DecodedresponseBody);
     // logger.d(responseBody);
     int statusCode = response.statusCode;
     //リクエスト成功時
